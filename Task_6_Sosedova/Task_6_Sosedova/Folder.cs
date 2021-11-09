8da903ccc8e35f013c62e3631f94531e10a24449
SosedovaViktoria <74018496+SosedovaViktoria@users.noreply.github.com>
2021-11-09 11:06:09 +0400

    Add files via upload

---
+++ Task_6_Sosedova/Task_6_Sosedova/Folder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6_Sosedova
+{
+    
+    [Serializable]
+    public class Folder
+    {
+        public List<string> Folders;
+        public Dictionary<string, string> Files;
+
+        [NonSerialized]
+        static Encoding encoding = Encoding.GetEncoding(1251);
+
+        public Folder(List<string> folders, Dictionary<string, string> files)
+        {
+            Folders = folders;
+            Files = files;
+        }
+
+        public Folder(string path)
+        {
+            Folders = GetFolders(path, path.Length);
+            Files = GetFilesList(path, path.Length);
+        }
+
+        public void DeployFolder(string roolPath)
+        {
+            foreach (string folder in Folders)
+            {
+                Directory.CreateDirectory(roolPath + folder);
+            }
+
+            foreach (var item in Files)
+            {
+                using (FileStream fs = new FileStream(roolPath + item.Key, FileMode.Create))
+                {
+                    byte[] bytes = encoding.GetBytes(item.Value);
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+        }
+
+        static List<string> GetFolders(string path, int initPathLength)
+        {
+            List<string> foldersList = new List<string>();
+            string[] dirs = Directory.GetDirectories(path);
+            foldersList.AddRange(dirs.Select(x => x.Remove(0, initPathLength)));
+
+            foreach (string subdirectory in dirs)
+            {
+                try
+                {
+                    foldersList.AddRange(GetFolders(subdirectory, initPathLength));
+                }
+                catch { }
+            }
+            return foldersList;
+        }
+
+        static Dictionary<string, string> GetFilesList(string path, int initPathLength)
+        {
+            Dictionary<string, string> files = new Dictionary<string, string>();
+            string[] dirs = Directory.GetDirectories(path);
+
+            foreach (string filename in Directory.GetFiles(path))
+            {
+                files.Add(filename.Remove(0, initPathLength), encoding.GetString(File.ReadAllBytes(filename)));
+            }
+
+            foreach (string subdirectory in dirs)
+            {
+                try
+                {
+                    GetFilesList(subdirectory, initPathLength).ToList().ForEach(x => files.Add(x.Key, x.Value));
+                }
+                catch { }
+            }
+            return files;
+        }
+    }
+}
---
+++ Task_6_Sosedova/Task_6_Sosedova/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Task_6_Sosedova
+{
+    class Program
+    {
+        static Encoding encoding = Encoding.GetEncoding(1251);
+        static string GetAlphabet()
+        {
+            StringBuilder res = new StringBuilder(255);
+            for (byte i = byte.MinValue; i < byte.MaxValue; i++){
+                res.Append(encoding.GetString(new byte[] { i }));}
+            return res.ToString();
+        }
+        static void Main(){
+            string abc = GetAlphabet();
+
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1. Шифрование каталога");
+            Console.WriteLine("2. Дешифрование каталога");
+            //все используемые файлы находятся Task_6_Sosedova->Task_6_Sosedova->bin->Debug
+            int checkbox = int.Parse(Console.ReadLine());
+            if (checkbox == 1)
+            {
+                Console.WriteLine("Введите адрес каталога для шифрования:");
+                string InFolder = Console.ReadLine();
+                //Для примера, каталог для шифрования: Folder_0 или Folder_1
+
+                Console.WriteLine("Введите ключ шифрования:");
+                string key = Console.ReadLine();//ввод ключа возможен, как на русском, так и на английском языке
+
+                Folder data = new Folder(InFolder);
+
+                string dataToCode;
+                using (MemoryStream Mstream = new MemoryStream())
+                {
+                    new BinaryFormatter().Serialize(Mstream, data);
+                    dataToCode = encoding.GetString(Mstream.ToArray());
+                }
+
+                using (StreamWriter sWriter = new StreamWriter("data.txt"))
+                {
+                    Vigenere vigenere = new Vigenere(abc);
+                    sWriter.Write(vigenere.Encrypt(dataToCode, key));
+                }
+
+                var info = new DirectoryInfo(InFolder);
+                info.Delete(true);
+            }
+            else if (checkbox == 2)
+            {
+
+                Console.WriteLine("Введите адрес файла каталога для расшифрования:");
+                string fileIn = Console.ReadLine();//файл для шифрования data.txt
+
+                Console.WriteLine("Введите ключ шифрования:");
+                string key = Console.ReadLine();
+
+                Console.WriteLine("Введите адрес развертывания каталога:");
+                string folderout = Console.ReadLine();
+                //В моем примере, данный каталог - Decryption
+
+                string dataToTransf;
+
+                using (StreamReader sReader = new StreamReader(fileIn))
+                {
+                    Vigenere vigenere = new Vigenere(abc);
+                    dataToTransf = vigenere.Decrypt(sReader.ReadToEnd(), key);
+                }
+                Folder data;
+                using (MemoryStream stream = new MemoryStream(encoding.GetBytes(dataToTransf)))
+                {
+                    data = (Folder)new BinaryFormatter().Deserialize(stream);
+                }
+
+                data.DeployFolder(folderout);
+
+                File.Delete(fileIn);
+            }
+        }
+    }
+    public class Vigenere
+    {
+        public string Alphabet { get; private set; }
+
+        public Vigenere(string abc)
+        {
+            if (abc == null || abc == string.Empty)
+            {
+                throw new Exception("Пустой алфавит!");
+            }
+            Alphabet = abc;
+        }
+
+        public string Encrypt(string text, string key)
+        {
+            int t = text.Length;
+            StringBuilder result = new StringBuilder(t);
+            var gamma = new StringBuilder(key).Insert(0, key, t).ToString().Substring(0, t);
+            for (int i = 0; i < t; i++)
+            {
+                if (Alphabet.IndexOf(text[i]) < 0)
+                {
+                    result.Append(text[i]);
+                }
+                else
+                {
+                    int b = Alphabet.Length + Alphabet.IndexOf(text[i]) + ((true ? 1 : -1) * Alphabet.IndexOf(gamma[i]));
+                    result.Append(Alphabet[(b) % Alphabet.Length]);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public string Decrypt(string text, string key)
+        {
+            int t = text.Length;
+            StringBuilder result = new StringBuilder(t);
+            while (key.Length < t)
+            {
+                key += key;
+            }
+
+            string gamma = key.Substring(0, t);
+
+            for (int i = 0; i < t; i++)
+            {
+                if (Alphabet.IndexOf(text[i]) < 0)
+                {
+                    result.Append(text[i]);
+                }
+                else
+                {
+                    int b = Alphabet.Length + Alphabet.IndexOf(text[i]) + ((false ? 1 : -1) * Alphabet.IndexOf(gamma[i]));
+                    result.Append(Alphabet[(b) % Alphabet.Length]);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
